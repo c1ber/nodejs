73e7da4274fbf66695f9cbc169a6479153c47396
Kaal Purush <kaalbd@gmail.com>
2013-09-08 00:45:54 +0600

    fix

--- imapevent/config.sample.js
+++ imapevent/config.sample.js
@@ -3,7 +3,7 @@ module.exports = {
 	imap_password: 'account_password',
 	client_id: 'numbers.apps.googleusercontent.com',
 	client_secret: 'apisecret',
-	redirect_url: 'http://localhost:8081/',
+	redirect_url: 'http://localhost:8081',
 	server_port: '8081',
 	server_host_ip: 'localhost'
 }
