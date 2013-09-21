35bea285d5f2c252d9c6ad48a7f287f9e82ebf3a
Kaal Purush <kaalbd@gmail.com>
2013-09-22 03:49:40 +0600

    nitrous.io! config

--- imapevent/config.sample.js
+++ imapevent/config.sample.js
@@ -1,9 +1,9 @@
 module.exports = {		
 	imap_user: 'name@gmail.com',
-	imap_password: 'account_password',
-	client_id: 'numbers.apps.googleusercontent.com',
-	client_secret: 'apisecret',
-	redirect_url: 'http://localhost:8081',
-	server_port: '8081',
-	server_host_ip: 'localhost'
+	imap_password: unescape('%escaped_password%'),
+	client_id: 'something.apps.googleusercontent.com',
+	client_secret: 'api_secret',
+	redirect_url: 'http://nodejs-00000.apse1.actionbox.io:3000',
+	server_port: '3000',
+	server_host_ip: '0.0.0.0'
 }
